d59a7881f0dd4897ebe5a09c2d71028777294c7b
Jesus Graterol <jesusgraterol.dev@protonmail.com>
2024-05-20 10:42:11 -0400

    Move the minifier to its own module
    Signed-off-by: Jesus Graterol <jesusgraterol.dev@protonmail.com>

--- src/build/build.ts
+++ src/build/build.ts
@@ -1,11 +1,24 @@
 import { IParsedArgs } from 'argv-utils';
 import { readTypeScriptConfigFile } from '../ts-config/index.js';
 import { compile } from '../compiler/compiler.js';
+import { minify } from '../minifier/index.js';
+
+/* ************************************************************************************************
+ *                                         IMPLEMENTATION                                         *
+ ************************************************************************************************ */
 
 /**
- * Run Build
- * Runs the Build Process that will be based on the project's tsconfig.json file.
+ * Runs the build process by reading the configuration file, compiling the project and lastly,
+ * minifying the outputs.
  * @param args
+ * @throws
+ * - if the provided path to the tsconfig.json file is invalid
+ * - if the tsconfig file has no content
+ * - if the tsconfig's content cannot be parsed
+ * - if the compilerOptions property is not present in the config object
+ * - if the outDir property is not present in the config object
+ * - if no minifiable files are found
+ * - if a file cannot be minified for any reason
  */
 const run = ({ tsconfig }: IParsedArgs) => {
   // read the tsconfig.json file
@@ -14,16 +27,16 @@ const run = ({ tsconfig }: IParsedArgs) => {
   // compile the project
   compile(config.compilerOptions.outDir, tsconfig);
 
-  // read the list of compiled files and uglify them
-  // minifyProject(config.compilerOptions.outDir);
+  // minify the compiled files
+  minify(config.compilerOptions.outDir);
 };
 
 
 
 
-/**
- * Module Exports
- */
+/* ************************************************************************************************
+ *                                         MODULE EXPORTS                                         *
+ ************************************************************************************************ */
 export {
   run,
 };
--- src/minifier/minifier.ts
+++ src/minifier/minifier.ts
@@ -1,36 +1,31 @@
 import {
-  rmSync,
   readFileSync,
   readdirSync,
   writeFileSync,
 } from 'node:fs';
 import { join } from 'node:path';
-import { execSync } from 'node:child_process';
 import { minify_sync } from 'terser';
 
-/* ***********************************************************************************************
- *                                      COMPILATION HELPERS                                      *
- *********************************************************************************************** */
-
-/**
- * Deletes the output directory if exists.
- */
-const cleanOutDir = (outDir: string) => rmSync(outDir, { recursive: true, force: true });
-
-/**
- * Compiles the project based on the tsconfig.json file.
- * @returns Buffer
- */
-const compileProject = (tsconfigPath: string): Buffer => execSync(`tsc --project "${tsconfigPath}"`);
+/* ************************************************************************************************
+ *                                         IMPLEMENTATION                                         *
+ ************************************************************************************************ */
 
 /**
  * Extracts the list of compiled JavaScript files present in the outDir.
  * @param {*} outDir
  * @returns string[]
+ * @throws
+ * - if no minifiable files are found
  */
 const __listMinifiableFiles = (outDir: string): string[] => {
-  const files = readdirSync(outDir, { recursive: true, encoding: 'utf-8' });
-  return files.filter((file) => /.js$/.test(file));
+  const files = readdirSync(
+    outDir,
+    { recursive: true, encoding: 'utf-8' },
+  ).filter((file) => /.js$/.test(file));
+  if (!files.length) {
+    throw new Error('The project could not be minified because no .js files were found in the output.');
+  }
+  return files;
 };
 
 /**
@@ -56,8 +51,11 @@ const __minifyFile = (codePath: string): string => {
 /**
  * Uglifies every compiled file (.js) from the outDir directory.
  * @param {*} outDir
+ * @throws
+ * - if no minifiable files are found
+ * - if a file cannot be minified for any reason
  */
-const minifyProject = (outDir: string) => {
+const minify = (outDir: string) => {
   __listMinifiableFiles(outDir).forEach((filePath) => {
     const codePath: string = join(outDir, filePath);
     writeFileSync(codePath, __minifyFile(codePath), 'utf8');
@@ -72,8 +70,5 @@ const minifyProject = (outDir: string) => {
  *                                         MODULE EXPORTS                                        *
  *********************************************************************************************** */
 export {
-  // compilation helpers
-  cleanOutDir,
-  compileProject,
-  minifyProject,
+  minify,
 };
